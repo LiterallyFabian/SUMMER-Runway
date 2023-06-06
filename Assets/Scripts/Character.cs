e7c43b2ffa89c76e97bf463bed3adb93b5b86e05
Fabian Lindgren <lindgrenfabian@gmail.com>
2023-06-06 16:28:51 +0200

    Add basic part switching

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// A character is a collection of 4 parts that can be switched out. Each part has X number of sprites that can be switched between.
@@ -10,24 +11,52 @@ public class Character : MonoBehaviour
     public KeyCode SwitchC;
     public KeyCode SwitchD;
 
-    [Header("CharacterParts")] public Sprite[] PartA;
+    [Header("Character parts")] public Sprite[] PartA;
     public Sprite[] PartB;
     public Sprite[] PartC;
     public Sprite[] PartD;
-    public SpriteRenderer RendererA;
-    public SpriteRenderer RendererB;
-    public SpriteRenderer RendererC;
-    public SpriteRenderer RendererD;
+    public Image ImageA;
+    public Image ImageB;
+    public Image ImageC;
+    public Image ImageD;
 
+    private readonly int[] _indexes = {0, 0, 0, 0};
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        UpdatePositions(); // default will always be 0000
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if ((Input.GetKeyDown(SwitchA) || Input.GetKeyDown(SwitchB) || Input.GetKeyDown(SwitchC) ||
+             Input.GetKeyDown(SwitchD)) == false)
+            return; // early return if no key was pressed
+
+        if (Input.GetKeyDown(SwitchA))
+            _indexes[0] = (_indexes[0] + 1) % PartA.Length;
+        if (Input.GetKeyDown(SwitchB))
+            _indexes[1] = (_indexes[1] + 1) % PartB.Length;
+        if (Input.GetKeyDown(SwitchC))
+            _indexes[2] = (_indexes[2] + 1) % PartC.Length;
+        if (Input.GetKeyDown(SwitchD))
+            _indexes[3] = (_indexes[3] + 1) % PartD.Length;
+
+        UpdatePositions();
+
+        Debug.Log($"{name}: {GetCombination()}");
+    }
+
+    private void UpdatePositions()
+    {
+        ImageA.sprite = PartA[_indexes[0]];
+        ImageB.sprite = PartB[_indexes[1]];
+        ImageC.sprite = PartC[_indexes[2]];
+        ImageD.sprite = PartD[_indexes[3]];
+    }
+
+    public int GetCombination()
+    {
+        return _indexes[0] * 1000 + _indexes[1] * 100 + _indexes[2] * 10 + _indexes[3]; // will make a number like 1234
     }
 }
